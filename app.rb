c82fff39f912dee7dff0793fe3b46bc032c63034
Sean Holstrom <basementbebop@gmail.com>
2015-01-17 22:47:15 -0800

    add stylesheet, update app.rb, update login.erb

--- app.rb
+++ app.rb
@@ -3,8 +3,15 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/place_of_waste')
 
-get('/') do
-	
-
-erb(:login)
+get('/login') do
+	@current_user = nil
+	erb(:login)
 end
+
+post('/add_user') do
+	@user_name = params['user_name']
+	@user_password = params['user_password']
+	@current_user = new.User({:user_name => @user_name, :user_password => @user_password})
+	redirect('/home')
+end
+# get('/home')
